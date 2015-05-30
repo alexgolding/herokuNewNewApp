17e91d62d25186928569918db6686a2ef141c5ba
Alex Golding <alex@ongozah.com>
2015-05-30 14:32:20 -0300

    Routes change

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'posts#index'
+  get 'public/test.html', to: 'welcome#index'
   #This is a comment
 
   # Example of regular route:
